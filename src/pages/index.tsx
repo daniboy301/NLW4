2adc5f95227428e96bcd0cc4cea80e639a612412
Daniel dos Santos <danielsantos30@outlook.com>
2021-03-11 13:14:16 -0300

    Componente desafios e interação entre os componentes com context api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/CountDown";
@@ -7,27 +8,42 @@ import { Profile } from "../components/Profile";
 import { ChalengeBox } from "../components/ChalengeBox";
 
 import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return (
     <div className={styles.container}>
       <Head>
         <title>Inicio | move.it</title>
       </Head>
 
+      <ExperienceBar />
 
-       <ExperienceBar />
-
-       <section>
-         <div>
-           <Profile />
-           <CompletedChallenges />
-           <Countdown />
-         </div>
-         <div>
-          <ChalengeBox/>
-         </div>
-       </section>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChalengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level,
+      currentExperience,
+      challengesCompleted
+    }
+  }
 }
